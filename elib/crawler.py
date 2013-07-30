32fc10c2fce528d389a85b541c443700032a3cc9
Yuri Astrov <yuriastrov@gmail.com>
2013-07-30 21:54:56 +0400

    In crawler: add logger, minor changes.

--- elib/crawler.py
+++ elib/crawler.py
@@ -3,30 +3,13 @@
 import os
 from elib.models import Book, Author, Genre
 from elib.manager import BookManager
-from io import BytesIO
-import elib.zipfile
+from zipper import InvalidZipFile, ZipReader
 try:
-  from lxml import etree
+    from lxml import etree
 except ImportError:
-  try:
-    # Python 2.5
-    import xml.etree.cElementTree as etree
-  except ImportError:
-    try:
-      # Python 2.5
-      import xml.etree.ElementTree as etree
-    except ImportError:
-      try:
-        # normal cElementTree install
-        import cElementTree as etree
-      except ImportError:
-        try:
-          # normal ElementTree install
-          import elementtree.ElementTree as etree
-        except ImportError:
-          print("Failed to import ElementTree from any known place")
+    print("Failed to import lxml module.")
 from sqlalchemy.orm.exc import NoResultFound
-import traceback
+import logging
 
 __doc__ = """Module for walking in library and makes indexing files
 
@@ -43,8 +26,9 @@ Example:
 
 class Crawler:
     """Crawler class for indexing ebooks."""
-    def __init__(self, path=None):
+    def __init__(self, path=None, logger=None):
         self.path = path
+        self.logger = logger or logging.getLogger(__name__)
     
     def registerbook(self, dict_):
         """Register book info in database."""
@@ -58,12 +42,13 @@ class Crawler:
             if info:
                 self.registerbook(info)
         except etree.XMLSyntaxError as e:
-            print("{}: {}".format(e, filename))
+            self.logger.warning("{}: {}".\
+                        format(e, filename))
         except InvalidZipFile as e:
-            print(e)
+            self.logger.warning(e)
         except Exception as e:
-            print("{}: {}".format(e, filename))
-            traceback.print_exc()
+            self.logger.exception("{}: {}".format(e, filename))
+
 
     def walk(self, path=None):
         """Main function for indexing path."""
@@ -77,45 +62,46 @@ class Crawler:
 
 
 class CrawlerManager(Crawler):
-    def __init__(self,  manager, path=None):
+    def __init__(self,  manager, path=None, logger=None):
         """Crawler or path for indexing"""
         self.manager = manager
         self.path = path
+        self.logger = logger or logging.getLogger(__name__)
+        self.session = self.manager.getsession()
 
     def registerbook(self, dict_):
         """Register one book info in database.
         dict_ - dictionary with book information"""
-        session = self.manager.getsession()
         a_list = []
         for element in dict_.authors:
             try:
-                a = Author.get(session,
+                a = Author.get(self.session,
                                 element["lastname"],
                                 element["firstname"])
             except NoResultFound:
                 a = Author(element["lastname"],
                            element["firstname"])
-            self.manager.add(a)
+            self.session.add(a)
             a_list.append(a)
 
         genre_list = []
         genres = dict_.genres
         for name in genres:
             try:
-                g = Genre.get(session, name)
+                g = Genre.get(self.session, name)
             except NoResultFound:
                 g = Genre(name)
-            self.manager.add(g)
+            self.session.add(g)
             genre_list.append(g)
         try:
-            b = Book.get(session, 
+            b = Book.get(self.session, 
                         dict_.title,dict_.lang, 
                         a_list, genre_list, dict_.path)
         except NoResultFound:
             b = Book(dict_.title,dict_.lang, 
                      a_list, genre_list, dict_.path)
-        self.manager.add(b)
-        self.manager.commit()
+        self.session.add(b)
+        self.session.commit()
     
     def run(self, path=None):
         """Register path"""
@@ -167,26 +153,21 @@ class BookInfo(dict):
         return self.__xml_to_dict(fname, fname)
 
     def __fromfb2zip(self, fname):
-        data = None
-        with zipfile.ZipFile(fname, "r") as zip:
-            error = zip.testzip()
+        with ZipReader(fname) as reader:
+            error = reader.testzip()
             if error:
-                #print("Bad file '%s' in archieve '%s'!"
-                #        %(error, zipname) )
-                #raise Exception("Bad file '%s' in archieve '%s'!".format(error, zipname))
-                raise InvalidZipFile(zipname,error)
-            name = zip.namelist()[0]
-            data = zip.read(name)
-        datafile = BytesIO(data)
+                raise InvalidZipFile(zipname, error)
+            for name in reader.namelist():
+                if name.endswith('.fb2'):
+                    datafile = reader.read(name)
         return self.__xml_to_dict(datafile, fname)
 
     def __fromepub(self, fname):
-        with zipfile.ZipFile(fname, "r") as zip:
-            error = zip.testzip()
+        with ZipReader(fname) as reader:
+            error = reader.testzip()
             if error:
-                raise InvalidZipFile(zipname,error)
-            data = zip.read('META-INF/container.xml')
-            datafile = BytesIO(data)
+                raise InvalidZipFile(zipname, error)
+            datafile = reader.read('META-INF/container.xml')
             tree = etree.parse(datafile)
             root = tree.getroot()
             ns = {'ns':'http://www.idpf.org/2007/opf',
@@ -194,13 +175,15 @@ class BookInfo(dict):
                   'n':'urn:oasis:names:tc:opendocument:xmlns:container',
                   'xsi':"http://www.w3.org/2001/XMLSchema-instance"}
             fullpath = tree.xpath('//n:rootfile',namespaces=ns)[0].get('full-path')
-            data = zip.read(fullpath)
-            datafile = BytesIO(data)
+            datafile = reader.read(fullpath)
             tree = etree.parse(datafile)
             root = tree.getroot()
             self.title = tree.xpath('//dc:title',namespaces=ns)[0].text
             author =tree.xpath('//dc:creator',namespaces=ns)[0].text
-            firstname, lastname = author.split()
+            try:
+                firstname, lastname = author.split()
+            except:
+                firstname, lastname = author
             self.authors = []
             d = {}
             d["firstname"] = firstname
@@ -226,19 +209,6 @@ class BookInfo(dict):
         else:
             return None
 
-
-class InvalidZipFile(Exception):
-    def __init__(self, filename, error):
-        """filename - file name of zip archive;
-        error - file name of first bad file in archive."""
-        self.filename = filename
-        self.error = error
-    def __repr__(self):
-        return "Bad file '{}' in archieve '{}'!".format(error, filename)
-    def __str__(self):
-        return repr(self)
-
-
 def main():
     import argparse
     parser = argparse.ArgumentParser()
@@ -257,7 +227,10 @@ def main():
         import config
         command = config.getSQLCommand()
     manager = BookManager(command)
-    cm = CrawlerManager(manager)
+    FORMAT = '%(levelname)s - %(message)s'
+    logging.basicConfig(format=FORMAT)
+    logger = logging.getLogger('Crawler.py')
+    cm = CrawlerManager(manager, logger=logger)
     cm.run(args.path)
 
 if __name__ == '__main__':
